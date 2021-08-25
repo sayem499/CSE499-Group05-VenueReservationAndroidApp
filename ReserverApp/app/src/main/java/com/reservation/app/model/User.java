0d94e1a179644e188790f24c4e229b9c64ee833c
fatemakishwar <fatema.kishwar@northsouth.edu>
2021-08-25 22:03:14 +0600

    Add User Model

---
+++ ReserverApp/app/src/main/java/com/reservation/app/model/User.java
@@ -0,0 +1,54 @@
+package com.reservation.app.model;
+
+/**
+ * @author Fatema
+ * since 8/25/21.
+ */
+public class User {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String photoUrl;
+    private String contactNumber;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+}
