48acc5c2851bd611cd8c2f638cd66f307d351ebb
sayem499 <79084263+sayem499@users.noreply.github.com>
2021-08-25 23:54:53 +0600

    #4 Designed User Profile

---
+++ ReserverApp/app/src/main/java/com/reservation/app/ui/Profile.java
@@ -0,0 +1,16 @@
+package com.reservation.app.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.reservation.app.R;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
