4908340c8426129e4ded73d2ec48bbd5103678af
fatemakishwar <fatema.kishwar@northsouth.edu>
2021-08-25 22:05:36 +0600

    Add Venue model

---
+++ ReserverApp/app/src/main/java/com/reservation/app/model/Venue.java
@@ -0,0 +1,83 @@
+package com.reservation.app.model;
+
+import java.util.List;
+
+/**
+ * @author Fatema
+ * since 8/25/21.
+ */
+public class Venue {
+
+    private String id;
+    private String name;
+    private String description;
+    private User owner;
+    private int seatCapacity;
+    private Double rentCost;
+    private List<String> photoUrls;
+    private List<String> slots;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public int getSeatCapacity() {
+        return seatCapacity;
+    }
+
+    public void setSeatCapacity(int seatCapacity) {
+        this.seatCapacity = seatCapacity;
+    }
+
+    public Double getRentCost() {
+        return rentCost;
+    }
+
+    public void setRentCost(Double rentCost) {
+        this.rentCost = rentCost;
+    }
+
+    public List<String> getPhotoUrls() {
+        return photoUrls;
+    }
+
+    public void setPhotoUrls(List<String> photoUrls) {
+        this.photoUrls = photoUrls;
+    }
+
+    public List<String> getSlots() {
+        return slots;
+    }
+
+    public void setSlots(List<String> slots) {
+        this.slots = slots;
+    }
+}
