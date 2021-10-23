43c3184d7166ef4b2f6b39c4c1818b77b4b831b5
sayem499 <79084263+sayem499@users.noreply.github.com>
2021-10-24 02:38:37 +0600

    #4 Linked Edit Profile Fragment With Profile

---
+++ ReserverApp/app/src/main/java/com/reservation/app/datasource/UserModel.java
@@ -0,0 +1,67 @@
+package com.reservation.app.datasource;
+
+public class UserModel {
+    private String userPhoneNumber;
+    private String userName;
+    private String userFirstName;
+    private String userLastName;
+    private String userEmail;
+    private String userAddress;
+    private String userProfilePictureUrl;
+
+    public String getUserPhoneNumber() {
+        return userPhoneNumber;
+    }
+
+    public void setUserPhoneNumber(String userPhoneNumber) {
+        this.userPhoneNumber = userPhoneNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserFirstName() {
+        return userFirstName;
+    }
+
+    public void setUserFirstName(String userFirstName) {
+        this.userFirstName = userFirstName;
+    }
+
+    public String getUserLastName() {
+        return userLastName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getUserProfilePictureUrl() {
+        return userProfilePictureUrl;
+    }
+
+    public void setUserProfilePictureUrl(String userProfilePictureUrl) {
+        this.userProfilePictureUrl = userProfilePictureUrl;
+    }
+}
--- ReserverApp/app/src/main/java/com/reservation/app/ui/Profile.java
+++ ReserverApp/app/src/main/java/com/reservation/app/ui/Profile.java
@@ -1,16 +1,25 @@
 package com.reservation.app.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.reservation.app.R;
 
 public class Profile extends AppCompatActivity {
 
+    DialogFragment dialogFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void launchEditProfileFragment(View view) {
+        dialogFragment = EditProfileFragment.newInstance("ON","OFF");
+        dialogFragment.show(getSupportFragmentManager(),"Edit Profile");
+
     }
 }
