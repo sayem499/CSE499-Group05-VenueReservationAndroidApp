980cffc29a1878815ab679f79048617cca116f54
Fatema Kishwar <fatema.kishwar@northsouth.edu>
2021-11-09 21:27:38 +0600

    Add home back button to AddVenue Screen

--- ReserverApp/app/src/main/java/com/reservation/app/ui/venue/add/AddVenueActivity.java
+++ ReserverApp/app/src/main/java/com/reservation/app/ui/venue/add/AddVenueActivity.java
@@ -1,10 +1,15 @@
 package com.reservation.app.ui.venue.add;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.reservation.app.R;
+
+import java.util.Objects;
 
 /**
  * @author Fatema
@@ -12,9 +17,25 @@ import com.reservation.app.R;
  */
 public class AddVenueActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_venue);
+
+        toolbar = findViewById(R.id.add_venue_toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
