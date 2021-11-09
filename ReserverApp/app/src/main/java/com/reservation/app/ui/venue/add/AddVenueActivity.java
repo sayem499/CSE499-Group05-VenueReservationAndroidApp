a7a97c2b7a483456a6de251e03054dd8bac80fbf
Fatema Kishwar <fatema.kishwar@northsouth.edu>
2021-11-10 00:37:06 +0600

    Implement venue image upload

--- ReserverApp/app/src/main/java/com/reservation/app/ui/venue/add/AddVenueActivity.java
+++ ReserverApp/app/src/main/java/com/reservation/app/ui/venue/add/AddVenueActivity.java
@@ -1,30 +1,48 @@
 package com.reservation.app.ui.venue.add;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.QuickContactBadge;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 import com.reservation.app.R;
+import com.reservation.app.datasource.UserProfilePicture;
 import com.reservation.app.datasource.VenueDataManager;
 import com.reservation.app.datasource.helper.RemoteResult;
 import com.reservation.app.model.Address;
 import com.reservation.app.model.Venue;
+import com.reservation.app.ui.Profile;
 import com.reservation.app.ui.util.DialogBuilder;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
 
 /**
  * @author Fatema
@@ -42,6 +60,11 @@ public class AddVenueActivity extends AppCompatActivity {
     private EditText edtCity;
     private EditText edtPostCode;
     private Button btnSaveVenue;
+    private FloatingActionButton floatingActionButton;
+    private ImageView imageViewVenue;
+    private Uri venueImageUri;
+    private String venueImageURL = "";
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +80,10 @@ public class AddVenueActivity extends AppCompatActivity {
         edtCity = findViewById(R.id.edt_city);
         edtPostCode = findViewById(R.id.edt_postcode);
         btnSaveVenue = findViewById(R.id.save_venue);
+        floatingActionButton = findViewById(R.id.add_venue_photo);
+        imageViewVenue = findViewById(R.id.venue_photo);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
         btnSaveVenue.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,9 +92,69 @@ public class AddVenueActivity extends AppCompatActivity {
             }
         });
 
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.with(AddVenueActivity.this)
+                        .crop()//Crop image(Optional), Check Customization for more option
+                        .compress(1024)            //Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)    //Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
+
         toolbar = findViewById(R.id.add_venue_toolbar);
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            //Image Uri will not be null for RESULT_OK
+            assert data != null;
+            venueImageUri = data.getData();
+            uploadVenuePhoto();
+            imageViewVenue.setImageURI(venueImageUri);
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void uploadVenuePhoto() {
+        final Dialog progressDialog = DialogBuilder.buildProgressDialog(this, "Uploading image");
+        progressDialog.show();
+
+        Random random = new Random();
+
+        if (venueImageUri != null && firebaseUser != null) {
+            StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Venue Pic");
+            final StorageReference fileRef = storageReference.child(firebaseUser.getPhoneNumber() + random.nextInt() + ".jpg");
+            StorageTask uploadTask = fileRef.putFile(venueImageUri);
+            uploadTask.continueWithTask(task -> {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+                return fileRef.getDownloadUrl();
+
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull @NotNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUrl = task.getResult();
+                        venueImageURL = downloadUrl.toString();
+
+                        progressDialog.dismiss();
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(AddVenueActivity.this, "Image not selected.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void saveVenue() {
@@ -92,14 +179,12 @@ public class AddVenueActivity extends AppCompatActivity {
 
         venue.setAddress(address);
 
-        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
         if (firebaseUser != null) {
             venue.setUserPhoneNumber(firebaseUser.getPhoneNumber());
         }
-        List<String> dummyPhotoUrls = new ArrayList<>();
-        dummyPhotoUrls.add("https://firebasestorage.googleapis.com/v0/b/reserver-app-f3003.appspot.com/o/rondhonu_1.jpeg?alt=media&token=ff2c6bf5-3e6f-4680-9069-0ff2a4f9b68c");
-        venue.setPhotoUrls(dummyPhotoUrls);
+        List<String> photoUrls = new ArrayList<>();
+        photoUrls.add(venueImageURL);
+        venue.setPhotoUrls(photoUrls);
 
         Dialog progressDialog = DialogBuilder.buildProgressDialog(this, "Saving...");
         progressDialog.show();
