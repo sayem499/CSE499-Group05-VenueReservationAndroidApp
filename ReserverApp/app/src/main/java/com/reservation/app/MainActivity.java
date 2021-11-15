c4c20d0bc64539ca495c1392a2e0a424ef9c1d7d
sayem499 <79084263+sayem499@users.noreply.github.com>
2021-11-16 04:27:23 +0600

    #10 Designed & Created Splash Screen

--- ReserverApp/app/src/main/java/com/reservation/app/MainActivity.java
+++ ReserverApp/app/src/main/java/com/reservation/app/MainActivity.java
@@ -5,13 +5,23 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.TextView;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.reservation.app.ui.Login;
 import com.reservation.app.viewmodel.AppViewModel;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppViewModel appViewModel;
+    private static int TIME1 = 5000;
+    TextView logoText1,logoText2;
+    private CircleImageView splashImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +31,27 @@ public class MainActivity extends AppCompatActivity {
                 .getInstance(this.getApplication())).get(AppViewModel.class);
         appViewModel.initUserData();
         appViewModel.initUserProfilePicture();
+        splashImage = findViewById(R.id.imageView6);
 
-        Intent intent = new Intent(getApplicationContext(), Login.class);
-        startActivity(intent);
-        finish();
+        logoText1 = findViewById(R.id.reserver_app_logo);
+        logoText2 = findViewById(R.id.textView8);
+
+        YoYo.with(Techniques.FadeIn).delay(100).duration(850).repeat(0).playOn(logoText1);
+        YoYo.with(Techniques.FadeIn).delay(100).duration(850).repeat(0).playOn(logoText2);
+        YoYo.with(Techniques.FadeOut).delay(4500).duration(800).repeat(0).playOn(logoText1);
+        YoYo.with(Techniques.FadeOut).delay(4500).duration(800).repeat(0).playOn(logoText2);
+
+
+
+
+
+        new Handler(Looper.myLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, TIME1);
     }
 }
